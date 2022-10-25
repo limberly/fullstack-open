6983ff6cc192bc38db56734c4e356e28d9ac50f3
Lim <limhyungue@gmail.com>
2022-10-25 14:54:27 -0400

    =

--- part1/examples/src/App.js
+++ part1/examples/src/App.js
@@ -1,14 +1,26 @@
 import { useState } from "react";
 
+const Display = ({ counter }) => <div>{counter}</div>;
+
+const Button = ({ onClick, text }) => {
+  return (
+    <button onClick={onClick}>
+      {text}
+    </button>
+  );
+};
+
 const App = () => {
   const [counter, setCounter] = useState(0);
   const increaseByOne = () => setCounter(counter + 1);
+  const decreaseByOne = () => setCounter(counter - 1);
   const resetToZero = () => setCounter(0);
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={increaseByOne}>plus</button>
-      <button onClick={resetToZero}>reset</button>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text='plus' />
+      <Button onClick={decreaseByOne} text='minus' />
+      <Button onClick={resetToZero} text='zero' />
     </div>
 
   )
